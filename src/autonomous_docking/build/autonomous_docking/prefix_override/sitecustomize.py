7f0f8fffd577a08d22113e115ef2e7006c9557bb
articlesmli <166521793+articlesmli@users.noreply.github.com>
2026-08-31 15:41:03 +0300

    Delete src directory

--- src/autonomous_docking/build/autonomous_docking/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ivanovaml/subsea_robotics_workspace/src/autonomous_docking/install/autonomous_docking'
