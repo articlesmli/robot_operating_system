935645ad2047a93d008ea803ca0c340745a6402b
ivanovaml <ivanovaml@hotmail.co.uk>
2026-08-31 16:06:41 +0300

    Stop tracking build, install, and log folders

--- build/sonar_data_pipeline/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ivanovaml/subsea_robotics_workspace/install/sonar_data_pipeline'
--- build/sonar_data_pipeline/share/sonar_data_pipeline/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/ivanovaml/subsea_robotics_workspace/build/sonar_data_pipeline"
